59e93918f5d170c1488bfb5404fe58d9c5651140
albert <63851587@qq.com>
2018-11-05 16:20:19 +0800

    update

--- src/RequestHandler.php
+++ src/RequestHandler.php
@@ -13,7 +13,6 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
-use rabbit\core\Context;
 use rabbit\core\ObjectFactory;
 
 /**
