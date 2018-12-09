122172720afcb46e1fbe6df9ff12feb98ecab7ee
albert <63851587@qq.com>
2018-12-09 23:47:27 +0800

    add udp server

---
+++ src/AbstractUdpServer.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/12/9
+ * Time: 22:04
+ */
+
+namespace rabbit\server;
+
+
+abstract class AbstractUdpServer extends Server
+{
+    /**
+     * @return \Swoole\Server
+     */
+    protected function createServer(): \Swoole\Server
+    {
+        return new \Swoole\Server($this->host, $this->port, $this->type, SWOOLE_SOCK_UDP);
+    }
+
+    /**
+     * @param \Swoole\Server|null $server
+     * @throws \DI\DependencyException
+     * @throws \DI\NotFoundException
+     */
+    protected function startServer(\Swoole\Server $server = null): void
+    {
+        parent::startServer($server);
+        $server->on('Packet', array($this, 'onPacket'));
+        $server->start();
+    }
+}
