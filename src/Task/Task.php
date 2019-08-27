e5d950a452cfc074a6ed5df8fa733317325cb7df
albert <63851587@qq.com>
2019-08-27 22:20:08 +0800

    fix bug

--- src/Task/Task.php
+++ src/Task/Task.php
@@ -55,7 +55,8 @@ class Task
      */
     public function task($data, int $dst_worker_id = -1, \Closure $function = null)
     {
-        return $function ? App::getServer()->getSwooleServer()->task($data, $dst_worker_id, $function) : App::getServer()->task($data,
+        return $function ? App::getServer()->getSwooleServer()->task($data, $dst_worker_id,
+            $function) : App::getServer()->getSwooleServer()->task($data,
             $dst_worker_id);
     }
 
