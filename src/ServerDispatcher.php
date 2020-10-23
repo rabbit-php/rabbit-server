9bd8d0bdc3c6a9a7a5dac96bfd75d5aeef81a54b
Albert <amuluowin@163.com>
2020-10-23 19:25:12 +0800

    opt code

--- src/ServerDispatcher.php
+++ src/ServerDispatcher.php
@@ -1,17 +1,16 @@
 <?php
+
 declare(strict_types=1);
 
 namespace Rabbit\Server;
 
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\ServerRequestInterface;
-use Psr\Http\Server\RequestHandlerInterface;
+use Throwable;
 use Rabbit\Base\Core\BaseObject;
 use Rabbit\Web\DispatcherInterface;
 use Rabbit\Web\ErrorHandlerInterface;
-use Rabbit\Web\RequestContext;
-use Rabbit\Web\ResponseContext;
-use Throwable;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 
 /**
  * Class ServerDispatcher
@@ -30,10 +29,8 @@ class ServerDispatcher extends BaseObject implements DispatcherInterface
      * @return ResponseInterface
      * @throws Throwable
      */
-    public function dispatch(ServerRequestInterface $request, ResponseInterface $response): ResponseInterface
+    public function dispatch(ServerRequestInterface $request, ResponseInterface $response): void
     {
-        RequestContext::set($request);
-        ResponseContext::set($response);
         try {
             // before dispatcher
             $requestHandler = clone $this->requestHandler;
@@ -43,9 +40,7 @@ class ServerDispatcher extends BaseObject implements DispatcherInterface
              * @var ErrorHandlerInterface $errorHandler
              */
             $errorHandler = getDI('errorHandler');
-            $response = $errorHandler->handle($throw);
+            $errorHandler->handle($throw, $response);
         }
-        $response->send();
-        return $response;
     }
 }
