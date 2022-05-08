3cea6f4b1297b0dbfc9ffd083e88470db78334a7
Albert <amuluowin@163.com>
2022-05-08 22:07:12 +0800

    fix key name

--- src/functions.php
+++ src/functions.php
@@ -8,13 +8,13 @@ use Rabbit\Server\ProcessShare;
 if (!function_exists('process_share')) {
     function process_share(string $key, callable $func, int $timeout = 3, string $type = 'share'): ProcessShare
     {
-        return ProcessShare::getShare($key, $timeout, $type)($func);
+        return ProcessShare::getShare("share-{$key}", $timeout, $type)($func);
     }
 }
 
 if (!function_exists('lock')) {
     function lock(string $key, callable $func, bool $next = true, int $timeout = 3): void
     {
-        ProcessLock::getLock($key, $timeout)($key, $func, $next, $timeout);
+        ProcessLock::getLock("lock-{$key}", $timeout)($key, $func, $next, $timeout);
     }
 }
